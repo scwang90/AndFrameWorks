5f546183ff5d029a52168d63f911e90a57536db0
scwang90 <scwang90@hotmail.com>
2015-04-11 21:13:57 +0800

    创建项目

---
+++ src/com/andwidget/satellitemenu/IDegreeProvider.java
@@ -0,0 +1,11 @@
+package com.andwidget.satellitemenu;
+
+/**
+ * Interface for providing degrees between satellites. 
+ * 
+ * @author Siyamed SINIR
+ *
+ */
+public interface IDegreeProvider {
+	public float[] getDegrees(int count, float totalDegrees);
+}
