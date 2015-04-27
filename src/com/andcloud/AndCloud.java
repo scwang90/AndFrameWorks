8331c3dbef2331286d3f55e3f31a8e24a156c3ef
Administrator <Administrator@pc201501040928>
2015-04-27 21:03:46 +0800

    更新Buger

--- src/com/andcloud/AndCloud.java
+++ src/com/andcloud/AndCloud.java
@@ -42,7 +42,7 @@ public class AndCloud {
 			AnalyticsConfig.setAppkey(appkey);
 			AnalyticsConfig.setChannel(channel);
 			MobclickAgent.setDebugMode(isDebug);
-//	       SDK在统计Fragment时，需要关闭Activity自带的页面统计，
+//			SDK在统计Fragment时，需要关闭Activity自带的页面统计，
 //			然后在每个页面中重新集成页面统计的代码(包括调用了 onResume 和 onPause 的Activity)。
 			MobclickAgent.openActivityDurationTrack(false);
 //			MobclickAgent.setAutoLocation(true);
