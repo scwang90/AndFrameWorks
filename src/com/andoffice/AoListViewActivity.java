d692ba82ec063f79f7b8b605442a17182b62ca7d
树朾 <scwang90@hotmail.com>
2015-07-03 16:44:14 +0800

    创建项目

---
+++ src/com/andoffice/AoListViewActivity.java
@@ -0,0 +1,489 @@
+package com.andoffice;
+
+import java.util.Date;
+import java.util.List;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListView;
+
+import com.andframe.activity.framework.AfActivity;
+import com.andframe.activity.framework.AfPageable;
+import com.andframe.adapter.AfListAdapter;
+import com.andframe.adapter.AfListAdapter.IAfLayoutItem;
+import com.andframe.bean.Page;
+import com.andframe.exception.AfException;
+import com.andframe.feature.AfIntent;
+import com.andframe.layoutbind.AfFrameSelector;
+import com.andframe.layoutbind.AfModuleNodata;
+import com.andframe.layoutbind.AfModuleProgress;
+import com.andframe.thread.AfListViewTask;
+import com.andframe.util.java.AfCollections;
+import com.andframe.view.AfListView;
+import com.andframe.view.pulltorefresh.AfPullToRefreshBase.OnRefreshListener;
+/**
+ * @Description: 数据列表框架 Activity
+ * @Author: scwang
+ * @Version: V1.0, 2015-2-27 上午10:27:22
+ * @param <T> 列表数据实体类
+ */
+public abstract class AoListViewActivity<T> extends AfActivity implements OnRefreshListener, OnItemClickListener, OnClickListener{
+
+	protected static final String EXTRA_LAYOUT = "EXTRA_LAYOUT";
+	
+	protected AfModuleNodata mNodata;
+	protected AfModuleProgress mProgress;
+	protected AfFrameSelector mSelector;
+
+	protected AfListView mListView;
+	protected AfListAdapter<T> mAdapter;
+	
+	protected boolean mPagingEnable = true;
+
+	/**
+	 * 缓存使用的 class 对象（json要用到）
+	 * 设置 并且任务为 TASK_LOAD AfListTask 将自动使用缓存功能
+	 */
+	public Class<T> mCacheClazz = null;
+	/** 
+	 *  缓存使用的 KEY_CACHELIST = this.getClass().getName()
+	 * 		KEY_CACHELIST 为缓存的标识
+	 */
+	public String KEY_CACHELIST = this.getClass().getName();
+	
+	public AoListViewActivity() {
+		// TODO Auto-generated constructor stub
+		
+	}
+	/**
+	 * 使用缓存必须调用这个构造函数
+	 * @param clazz
+	 */
+	public AoListViewActivity(Class<T> clazz) {
+		// TODO Auto-generated constructor stub
+		this.mCacheClazz = clazz;
+	}
+	/**
+	 * 使用缓存必须调用这个构造函数
+	 * 	可以自定义缓存标识
+	 * @param clazz
+	 */
+	public AoListViewActivity(Class<T> clazz, String KEY_CACHELIST) {
+		// TODO Auto-generated constructor stub
+		this.mCacheClazz = clazz;
+		this.KEY_CACHELIST = KEY_CACHELIST;
+	}
+	/**
+	 * @Description: 创建方法
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:28:01
+	 * @Modified:
+	 * @param bundle
+	 * @param intent
+	 * @throws Exception
+	 */
+	protected void onCreate(Bundle bundle, AfIntent intent) throws Exception {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle, intent);
+		setContentView(getLayoutId());
+
+		mNodata = newModuleNodata(this);
+		mProgress = newModuleProgress(this);
+		mSelector = newAfFrameSelector(this);
+
+		mListView = new AfListView(findListView(this));
+		mListView.setOnRefreshListener(this);
+		mListView.setOnItemClickListener(this);
+		
+		// 设置banner尺寸
+		setLoading();
+		postTask(new AbListViewTask(mCacheClazz,KEY_CACHELIST));
+	}
+	/**
+	 * @Description: 获取setContentView的id
+	 * @Author: scwang
+	 * @return id
+	 */
+	protected abstract int getLayoutId();
+	/**
+	 * 获取列表控件
+	 * @param pageable
+	 * @return pageable.findListViewById(id)
+	 */
+	protected abstract ListView findListView(AfPageable pageable);
+
+	/**
+	 * 新建页面选择器
+	 * @param pageable
+	 * @return
+	 */
+	protected abstract AfFrameSelector newAfFrameSelector(AfPageable pageable);
+	/**
+	 * 新建加载页面
+	 * @param pageable
+	 * @return
+	 */
+	protected abstract AfModuleProgress newModuleProgress(AfPageable pageable);
+	/**
+	 * 新建空数据页面
+	 * @param pageable
+	 * @return
+	 */
+	protected abstract AfModuleNodata newModuleNodata(AfPageable pageable);
+
+
+	/**
+	 * @Description: 显示数据页面
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:18:26
+	 * @param adapter
+	 */
+	public void setData(AfListAdapter<T> adapter) {
+		// TODO Auto-generated method stub
+		mListView.setAdapter(adapter);
+		mSelector.SelectFrame(mListView);
+	}
+
+	/**
+	 * @Description: 正在加载数据提示
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:18:05
+	 */
+	public void setLoading() {
+		// TODO Auto-generated method stub
+		mProgress.setDescription("正在加载...");
+		mSelector.SelectFrame(mProgress);
+	}
+	
+	/**
+	 * 空数据页面刷新监听器
+	 * 子类需要重写监听器的话可以对 
+	 * mNodataRefreshListener 重新赋值
+	 */
+	private OnClickListener mNodataRefreshListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			onRefresh();
+			setLoading();
+		}
+	};
+
+	/**
+	 * @Description: 处理空数据
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:16:46
+	 * @Modified: 初次创建setNodata方法
+	 */
+	public void setNodata() {
+		// TODO Auto-generated method stub
+		mNodata.setDescription("抱歉，暂无数据");
+		mSelector.SelectFrame(mNodata);
+		mNodata.setOnRefreshListener(mNodataRefreshListener);
+	}
+
+	/**
+	 * @Description: 错误信息处理
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:16:26
+	 * @param ex
+	 */
+	public void setLoadError(Throwable ex) {
+		// TODO Auto-generated method stub
+		mNodata.setDescription(AfException.handle(ex, "数据加载出现异常"));
+		mNodata.setOnRefreshListener(mNodataRefreshListener);
+		mSelector.SelectFrame(mNodata);
+	}
+
+	/**
+	 * @Description: 用户加载分页通知事件
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:15:54
+	 * @return
+	 */
+	@Override
+	public boolean onMore() {
+		// TODO Auto-generated method stub
+		postTask(new AbListViewTask(mAdapter));
+		return true;
+	}
+
+	/**
+	 * @Description: 用户刷新数据通知事件
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:14:52
+	 * @return
+	 */
+	@Override
+	public boolean onRefresh() {
+		// TODO Auto-generated method stub
+		postTask(new AbListViewTask(null));
+		return true;
+	}
+
+	/**
+	 * @Description: 数据列表点击事件
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:25:18
+	 * @param parent
+	 * @param view
+	 * @param index
+	 * @param id
+	 */
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, long id, int index) {
+		// TODO Auto-generated method stub
+		T model = mAdapter.getItemAt(mListView.getDataIndex(index));
+		onItemClick(model,index);
+	}
+
+	/**
+	 * @Description: onItemClick 事件的 包装 一般情况下子类可以重写这个方法
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:20:05
+	 * @param model
+	 * @param index
+	 */
+	protected void onItemClick(T model, int index) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+//		if (v != null && v.getId() == ModuleNodata.ID_BUTTON) {
+//			onRefresh();
+//			setLoading();
+//		}
+	}
+
+	/**
+	 * 数据加载内部任务类（数据加载事件已经转发，无实际处理代码）
+	 * @author scwang
+	 */
+	protected class AbListViewTask extends AfListViewTask<T> {
+
+		/**
+		 * 可以触发加载缓存任务（框架缓存加载失败会改变成刷新任务）
+		 * @param clazz
+		 * @param KEY_CACHELIST
+		 */
+		public AbListViewTask(Class<T> clazz, String KEY_CACHELIST) {
+			super(clazz, KEY_CACHELIST);
+			// TODO Auto-generated constructor stub
+		}
+
+		/**
+		 * 可以触发加载更多（分页）任务 （传空null可以触发刷新任务）
+		 * @param adapter 适配器，用于统计当前条数计算分页（传空null可以触发刷新任务）
+		 */
+		public AbListViewTask(AfListAdapter<T> adapter) {
+			super(adapter);
+			// TODO Auto-generated constructor stub
+		}
+
+		@Override
+		protected List<T> onLoad() {
+			// TODO Auto-generated method stub
+			List<T> list = AoListViewActivity.this.onTaskLoad();
+			if (!AfCollections.isEmpty(list)) {
+				return list;
+			}
+			return super.onLoad();
+		}
+
+		//事件转发 参考 AfListViewFrament.onListByPage
+		@Override
+		protected List<T> onListByPage(Page page, int task) throws Exception {
+			// TODO Auto-generated method stub
+			return AoListViewActivity.this.onTaskListByPage(page,task);
+		}
+
+		//事件转发 参考 AfListViewFrament.onLoaded
+		@Override
+		protected boolean onLoaded(boolean isfinish, List<T> ltdata,Date cachetime) {
+			// TODO Auto-generated method stub
+			return AoListViewActivity.this.onLoaded(this,isfinish, ltdata,cachetime);
+		}
+
+		//事件转发 参考 AfListViewFrament.onRefreshed
+		@Override
+		protected boolean onRefreshed(boolean isfinish, List<T> ltdata) {
+			// TODO Auto-generated method stub
+			return AoListViewActivity.this.onRefreshed(this,isfinish, ltdata);
+		}
+		//事件转发 参考 AfListViewFrament.onMored
+		@Override
+		protected boolean onMored(boolean isfinish, List<T> ltdata,
+				boolean ended) {
+			// TODO Auto-generated method stub
+			return AoListViewActivity.this.onMored(this,isfinish, ltdata);
+		}
+
+	}
+	
+
+	/**
+	 * @Description: 缓存加载结束处理时间（框架默认调用onRefreshed事件处理）
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-1-22 下午9:19:02
+	 * @param task 
+	 * @param isfinish
+	 * @param ltdata
+	 * @param cachetime 缓存时间
+	 * @return
+	 */
+	protected boolean onLoaded(AbListViewTask task, boolean isfinish,
+			List<T> ltdata, Date cachetime) {
+		// TODO Auto-generated method stub
+		boolean deal = onRefreshed(task,isfinish,ltdata);
+		if (isfinish && !AfCollections.isEmpty(ltdata)) {
+			//设置上次刷新缓存时间
+			mListView.setLastUpdateTime(cachetime);
+		}
+		return deal;
+	}
+	/**
+	 * @param task 
+	 * @Description: 任务刷新结束处理事件
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-1-20 上午11:29:09
+	 * @param isfinish 是否成功执行
+	 * @param ltdata
+	 * @return 返回true 已经做好错误页面显示 返回false 框架会做好默认错误反馈
+	 */
+	@SuppressWarnings("static-access")
+	protected boolean onRefreshed(AbListViewTask task, boolean isfinish, List<T> ltdata) {
+		// TODO Auto-generated method stub
+		if (isfinish) {
+			//通知列表刷新完成
+			mListView.finishRefresh();
+			if (!AfCollections.isEmpty(ltdata)) {
+				mAdapter = newAdapter(getActivity(), ltdata);
+				setData(mAdapter);
+				if (ltdata.size() < task.PAGE_SIZE && mPagingEnable) {
+					mListView.removeMoreView();
+				}else {
+					mListView.addMoreView();
+				}
+			} else {
+				setNodata();
+			}
+		} else {
+			//通知列表刷新失败
+			mListView.finishRefreshFail();
+			if (mAdapter != null && mAdapter.getCount() > 0) {
+				mListView.setAdapter(mAdapter);
+				mSelector.SelectFrame(mListView);
+				makeToastLong(task.makeErrorToast("加载失败"));
+			} else {
+				setLoadError(task.mException);
+			}
+		}
+		return true;
+	}
+	/**
+	 * @Description: 任务加载更多结束处理事件
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-1-20 上午11:34:04
+	 * @param loadListTask
+	 * @param isfinish
+	 * @param ltdata
+	 * @return 返回true 已经做好错误页面显示 返回false 框架会做好默认错误反馈
+	 */
+	@SuppressWarnings("static-access")
+	protected boolean onMored(AbListViewTask task, boolean isfinish,
+			List<T> ltdata) {
+		// TODO Auto-generated method stub
+		// 通知列表刷新完成
+		mListView.finishLoadMore();
+		if (isfinish) {
+			if (!AfCollections.isEmpty(ltdata)) {
+				final int count = mAdapter.getCount();
+				// 更新列表
+				mAdapter.addData(ltdata);
+				mListView.smoothScrollToPosition(count+1);
+			}
+			if (ltdata.size() < task.PAGE_SIZE) {
+				// 关闭更多选项
+				makeToastShort("数据全部加载完毕！");
+				mListView.removeMoreView();
+			}
+		} else {
+			makeToastLong(task.makeErrorToast("获取更多失败！"));
+		}
+		return true;
+	}
+	/**
+	 * 获取列表项布局Item
+	 * 如果重写 newAdapter 之后，本方法将无效
+	 * @param data 对应的数据
+	 * @return 实现 布局接口 IAfLayoutItem 的Item兑现
+	 * 	new LayoutItem implements IAfLayoutItem<T>(){}
+	 */
+	protected abstract IAfLayoutItem<T> getItemLayout(T data);
+
+	/**
+	 * @Description: 加载缓存列表（不分页，在异步线程中执行，不可以更改页面操作）
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-1-22 下午9:27:48
+	 * @Modified: 初次创建onLoad方法
+	 * @return 返回 null 可以使用框架内置缓存
+	 */
+	protected List<T> onTaskLoad() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	/**
+	 * 数据分页加载（在异步线程中执行，不可以更改页面操作）
+	 * @param page 分页对象
+	 * @param task 任务id
+	 * @return 加载到的数据列表
+	 * @throws Exception
+	 */
+	protected abstract List<T> onTaskListByPage(Page page, int task) throws Exception;
+
+	/**
+	 * @Description: 根据数据ltdata新建一个 适配器 重写这个方法之后getItemLayout方法将失效
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-1-20 下午4:46:34
+	 * @Modified: 初次创建newAdapter方法
+	 * @param context
+	 * @param ltdata
+	 * @return
+	 */
+	protected AfListAdapter<T> newAdapter(Context context, List<T> ltdata) {
+		// TODO Auto-generated method stub
+		return new AbListViewAdapter(getContext(), ltdata);
+	}
+
+	/**
+	 * @Description: ListView数据适配器（事件已经转发getItemLayout，无实际处理代码）
+	 * @Author: scwang
+	 * @Version: V1.0, 2015-2-27 上午10:21:27
+	 */
+	protected class AbListViewAdapter extends AfListAdapter<T>{
+
+		public AbListViewAdapter(Context context, List<T> ltdata) {
+			super(context, ltdata);
+			// TODO Auto-generated constructor stub
+		}
+		/**
+		 * @Description: 转发事件到 AoListViewActivity.this.getItemLayout(data);
+		 * @Author: scwang
+		 * @param data
+		 * @return 
+		 */
+		@Override
+		protected IAfLayoutItem<T> getItemLayout(T data) {
+			// TODO Auto-generated method stub
+			return AoListViewActivity.this.getItemLayout(data);
+		}
+		
+	}
+}
---
+++ src/com/andoffice/AoListViewActivityImpl.java
@@ -0,0 +1,94 @@
+package com.andoffice;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.andframe.activity.framework.AfPageable;
+import com.andframe.feature.AfIntent;
+import com.andframe.layoutbind.AfFrameSelector;
+import com.andframe.layoutbind.AfModuleNodata;
+import com.andframe.layoutbind.AfModuleNodataImpl;
+import com.andframe.layoutbind.AfModuleProgress;
+import com.andframe.layoutbind.AfModuleProgressImpl;
+import com.andframe.layoutbind.AfModuleTitlebar;
+/**
+ * @Description: 数据列表框架 Activity
+ * @Author: scwang
+ * @Version: V1.0, 2015-2-27 上午10:27:22
+ * @param <T> 列表数据实体类
+ */
+public abstract class AoListViewActivityImpl<T> extends AoListViewActivity<T>{
+	
+	protected AfModuleTitlebar mTitlebar;
+
+	public AoListViewActivityImpl() {
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * 使用缓存必须调用这个构造函数
+	 * @param clazz
+	 */
+	public AoListViewActivityImpl(Class<T> clazz) {
+		super(clazz);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * 使用缓存必须调用这个构造函数
+	 * 	可以自定义缓存标识
+	 * @param clazz
+	 */
+	public AoListViewActivityImpl(Class<T> clazz, String KEY_CACHELIST) {
+		super(clazz,KEY_CACHELIST);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onCreate(Bundle bundle, AfIntent intent) throws Exception {
+		// TODO Auto-generated method stub
+		super.onCreate(bundle, intent);
+		mTitlebar = new AfModuleTitlebar(this);
+	}
+	
+	@Override
+	protected int getLayoutId() {
+		// TODO Auto-generated method stub
+		return R.layout.af_activity_listview;
+	}
+
+	@Override
+	protected ListView findListView(AfPageable pageable) {
+		// TODO Auto-generated method stub
+		return pageable.findViewByID(R.id.modulelistview_listview);
+	}
+
+	@Override
+	protected AfFrameSelector newAfFrameSelector(AfPageable pageable) {
+		// TODO Auto-generated method stub
+		return new AfFrameSelector(this, R.id.modulelistview_contentframe);
+	}
+
+	@Override
+	protected AfModuleProgress newModuleProgress(AfPageable pageable) {
+		// TODO Auto-generated method stub
+		return new AfModuleProgressImpl(pageable);
+	}
+
+	@Override
+	protected AfModuleNodata newModuleNodata(AfPageable pageable) {
+		// TODO Auto-generated method stub
+		return new AfModuleNodataImpl(pageable);
+	}
+
+//	@Override
+//	protected IAfLayoutItem<T> getItemLayout(T data) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+//	@Override
+//	protected List<T> onTaskListByPage(Page page, int task) throws Exception {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
+}
