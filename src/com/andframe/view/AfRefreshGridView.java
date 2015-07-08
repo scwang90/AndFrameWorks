d743048b26da4e97f894a08c4771ba693e7fd4ba
树朾 <scwang90@hotmail.com>
2015-07-09 00:58:46 +0800

    解决列表刷新问题

--- src/com/andframe/view/AfRefreshGridView.java
+++ src/com/andframe/view/AfRefreshGridView.java
@@ -1,10 +1,9 @@
 package com.andframe.view;
 
-import java.lang.reflect.Field;
-
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.AbsListView;
+import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.GridView;
@@ -45,53 +44,112 @@ public abstract class AfRefreshGridView extends AfPullToRefreshBase<GridView> {
 		return new GridView(context, attrs);
 	}
 
-	@Override
-	protected final boolean isReadyForPullDown() {
-		// TODO Auto-generated method stub
-		// targetview.getOverScrollMode();
-		return mIsOpenRefresh
-				&& 5 >= Math.abs(getFirstPositionDistanceGuess(mTargetView)
-						- mTargetView.getTop());
-	}
+//	@Override
+//	protected final boolean isReadyForPullDown() {
+//		// TODO Auto-generated method stub
+//		// targetview.getOverScrollMode();
+//		return mIsOpenRefresh
+//				&& 5 >= Math.abs(getFirstPositionDistanceGuess(mTargetView)
+//						- mTargetView.getTop());
+//	}
+//
+//	@Override
+//	protected final boolean isReadyForPullUp() {
+//		// TODO Auto-generated method stub
+//		return mIsNeedFooter
+//				&& 5 >= Math.abs(getLastPositionDistanceGuess(mTargetView)
+//						- mTargetView.getBottom());
+//	}
+//
+//	int getFirstPositionDistanceGuess(AbsListView view) {
+//		Field field;
+//		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
+//		try {
+//			field = AbsListView.class
+//					.getDeclaredField("mFirstPositionDistanceGuess");
+//			field.setAccessible(true);
+//			return (Integer) field.get(view);
+//		} catch (Throwable e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return -1;
+//	}
+//
+//	int getLastPositionDistanceGuess(AbsListView view) {
+//		Field field;
+//		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
+//		try {
+//			field = AbsListView.class
+//					.getDeclaredField("mLastPositionDistanceGuess");
+//			field.setAccessible(true);
+//			return (Integer) field.get(view);
+//		} catch (Throwable e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return -1;
+//
+//	}
 
 	@Override
-	protected final boolean isReadyForPullUp() {
-		// TODO Auto-generated method stub
-		return mIsNeedFooter
-				&& 5 >= Math.abs(getLastPositionDistanceGuess(mTargetView)
-						- mTargetView.getBottom());
+	protected boolean isReadyForPullDown() {
+		return mIsOpenRefresh&&isFirstItemVisible();
 	}
 
-	int getFirstPositionDistanceGuess(AbsListView view) {
-		Field field;
-		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
-		try {
-			field = AbsListView.class
-					.getDeclaredField("mFirstPositionDistanceGuess");
-			field.setAccessible(true);
-			return (Integer) field.get(view);
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	@Override
+	protected boolean isReadyForPullUp() {
+		return mIsNeedFooter&&isLastItemVisible();
+	}
+	
+	private boolean isFirstItemVisible() {
+		final Adapter adapter = mTargetView.getAdapter();
+		if (null == adapter || adapter.isEmpty()) {
+			return true;
+		} else {
+			/**
+			 * This check should really just be:
+			 * mRefreshableView.getFirstVisiblePosition() == 0, but PtRListView
+			 * internally use a HeaderView which messes the positions up. For
+			 * now we'll just add one to account for it and rely on the inner
+			 * condition which checks getTop().
+			 */
+			if (mTargetView.getFirstVisiblePosition() <= 1) {
+				final View firstVisibleChild = mTargetView.getChildAt(0);
+				if (firstVisibleChild != null) {
+					return firstVisibleChild.getTop() >= mTargetView.getTop();
+				}
+			}
 		}
-		return -1;
+		return false;
 	}
-
-	int getLastPositionDistanceGuess(AbsListView view) {
-		Field field;
-		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
-		try {
-			field = AbsListView.class
-					.getDeclaredField("mLastPositionDistanceGuess");
-			field.setAccessible(true);
-			return (Integer) field.get(view);
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	
+	private boolean isLastItemVisible() {
+		final Adapter adapter = mTargetView.getAdapter();
+		if (null == adapter || adapter.isEmpty()) {
+			return true;
+		} else {
+			final int lastItemPosition = mTargetView.getCount() - 1;
+			final int lastVisiblePosition = mTargetView.getLastVisiblePosition();
+			/**
+			 * This check should really just be: lastVisiblePosition ==
+			 * lastItemPosition, but PtRListView internally uses a FooterView
+			 * which messes the positions up. For me we'll just subtract one to
+			 * account for it and rely on the inner condition which checks
+			 * getBottom().
+			 */
+			if (lastVisiblePosition >= lastItemPosition - 1) {
+				final int childIndex = lastVisiblePosition - mTargetView.getFirstVisiblePosition();
+				final View lastVisibleChild = mTargetView.getChildAt(childIndex);
+				if (lastVisibleChild != null) {
+					return lastVisibleChild.getBottom() <= mTargetView.getBottom();
+				}
+			}
 		}
-		return -1;
-
+		return false;
 	}
+	
+
 
 	public void setAdapter(ListAdapter mAdapter) {
 		// TODO Auto-generated method stub
--- src/com/andframe/view/AfRefreshListView.java
+++ src/com/andframe/view/AfRefreshListView.java
@@ -1,16 +1,14 @@
 package com.andframe.view;
 
-import java.lang.reflect.Field;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build.VERSION;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
-import android.widget.AbsListView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -132,52 +130,111 @@ public abstract class AfRefreshListView<T extends ListView> extends AfPullToRefr
 //				mTargetView.getLastVisiblePosition() 
 //				== mTargetView.getCount() - 1;
 //	}
-	@Override
-	protected final boolean isReadyForPullDown() {
-		// TODO Auto-generated method stub
-		// targetview.getOverScrollMode();
-		return mIsOpenRefresh
-				&& 5 >= Math.abs(getFirstPositionDistanceGuess(mTargetView)
-						- mTargetView.getTop());
-	}
+	
+//	@Override
+//	protected final boolean isReadyForPullDown() {
+//		// TODO Auto-generated method stub
+//		// targetview.getOverScrollMode();
+//		return mIsOpenRefresh
+//				&& 5 >= Math.abs(getFirstPositionDistanceGuess(mTargetView)
+//						- mTargetView.getTop());
+//	}
+//
+//	@Override
+//	protected final boolean isReadyForPullUp() {
+//		// TODO Auto-generated method stub
+//		return mIsNeedFooter
+//				&& 5 >= Math.abs(getLastPositionDistanceGuess(mTargetView)
+//						- mTargetView.getBottom());
+//	}
+//
+//	int getFirstPositionDistanceGuess(AbsListView view) {
+//		Field field;
+//		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
+//		try {
+//			field = AbsListView.class
+//					.getDeclaredField("mFirstPositionDistanceGuess");
+//			field.setAccessible(true);
+//			return (Integer) field.get(view);
+//		} catch (Throwable e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return -1;
+//	}
+//
+//	int getLastPositionDistanceGuess(AbsListView view) {
+//		Field field;
+//		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
+//		try {
+//			field = AbsListView.class
+//					.getDeclaredField("mLastPositionDistanceGuess");
+//			field.setAccessible(true);
+//			return (Integer) field.get(view);
+//		} catch (Throwable e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return -1;
+//
+//	}
 
 	@Override
-	protected final boolean isReadyForPullUp() {
-		// TODO Auto-generated method stub
-		return mIsNeedFooter
-				&& 5 >= Math.abs(getLastPositionDistanceGuess(mTargetView)
-						- mTargetView.getBottom());
+	protected boolean isReadyForPullDown() {
+		return mIsOpenRefresh&&isFirstItemVisible();
 	}
 
-	int getFirstPositionDistanceGuess(AbsListView view) {
-		Field field;
-		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
-		try {
-			field = AbsListView.class
-					.getDeclaredField("mFirstPositionDistanceGuess");
-			field.setAccessible(true);
-			return (Integer) field.get(view);
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return -1;
+	@Override
+	protected boolean isReadyForPullUp() {
+		return mIsNeedFooter&&isLastItemVisible();
 	}
+	
+	private boolean isFirstItemVisible() {
+		final Adapter adapter = mTargetView.getAdapter();
 
-	int getLastPositionDistanceGuess(AbsListView view) {
-		Field field;
-		// 获取ContextWrapper对象中的mBase变量。该变量保存了ContextImpl对象
-		try {
-			field = AbsListView.class
-					.getDeclaredField("mLastPositionDistanceGuess");
-			field.setAccessible(true);
-			return (Integer) field.get(view);
-		} catch (Throwable e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (null == adapter || adapter.isEmpty()) {
+			return true;
+		} else {
+			/**
+			 * This check should really just be:
+			 * mRefreshableView.getFirstVisiblePosition() == 0, but PtRListView
+			 * internally use a HeaderView which messes the positions up. For
+			 * now we'll just add one to account for it and rely on the inner
+			 * condition which checks getTop().
+			 */
+			if (mTargetView.getFirstVisiblePosition() <= 1) {
+				final View firstVisibleChild = mTargetView.getChildAt(0);
+				if (firstVisibleChild != null) {
+					return firstVisibleChild.getTop() >= mTargetView.getTop();
+				}
+			}
 		}
-		return -1;
-
+		return false;
+	}
+	
+	private boolean isLastItemVisible() {
+		final Adapter adapter = mTargetView.getAdapter();
+		if (null == adapter || adapter.isEmpty()) {
+			return true;
+		} else {
+			final int lastItemPosition = mTargetView.getCount() - 1;
+			final int lastVisiblePosition = mTargetView.getLastVisiblePosition();
+			/**
+			 * This check should really just be: lastVisiblePosition ==
+			 * lastItemPosition, but PtRListView internally uses a FooterView
+			 * which messes the positions up. For me we'll just subtract one to
+			 * account for it and rely on the inner condition which checks
+			 * getBottom().
+			 */
+			if (lastVisiblePosition >= lastItemPosition - 1) {
+				final int childIndex = lastVisiblePosition - mTargetView.getFirstVisiblePosition();
+				final View lastVisibleChild = mTargetView.getChildAt(childIndex);
+				if (lastVisibleChild != null) {
+					return lastVisibleChild.getBottom() <= mTargetView.getBottom();
+				}
+			}
+		}
+		return false;
 	}
 	
 	public final void addMoreView() {
