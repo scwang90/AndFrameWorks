5923e0773ec0c7a3110fe1a71328230ace6eb1e4
树朾 <scwang90@hotmail.com>
2015-07-23 01:26:22 +0800

    抛弃Jsoup的依赖实现，更改基于Restfu和正则表达式

--- src/com/andaddress/AddressKernel.java
+++ src/com/andaddress/AddressKernel.java
@@ -1,45 +1,60 @@
 package com.andaddress;
 
 import java.io.IOException;
-
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.andframe.application.AfApplication;
-import com.andframe.helper.android.AfDesHelper;
-import com.andframe.util.java.AfMD5;
+import com.andrestrequest.AndRestConfig;
+import com.andrestrequest.http.DefaultRequestHandler;
+import com.andrestrequest.http.DefaultRequestHandler.HttpMethod;
+import com.andrestrequest.http.Response;
 
 public class AddressKernel {
+	
+	private static final String NOT_FIND = "未获取";
+	private static boolean found = false;
+	private static boolean loading = false;
+	private static String address = NOT_FIND;
+	private static String city = NOT_FIND;
+	private static String operator = NOT_FIND;
+	
+	private static String url = "http://www.ip138.com";
+//	private static String url = "1591b7dba62df3fc2a5fe7ccfe6a22ae9bafcfaefbd94b1d";
 
-	//private static String url = "http://iframe.ip138.com/ic.asp";
-	private static String url = "1591b7dba62df3fc2a5fe7ccfe6a22ae9bafcfaefbd94b1d";
-	//"http://www.ip138.com/";
-	private static Document mDocument;
-
-	static {
-		try {
-			String key = AfMD5.getMD5("");
-			AfDesHelper helper = new AfDesHelper(key );
-			url = helper.decrypt(url);
-		} catch (Exception e) {
-			// TODO: handle exception
+	private static void loading(){
+		if (!loading) {
+			loading = true;
+			new Thread() {
+				public void run() {
+					try {
+						work();
+						AfApplication.getApp().onUpdateAppinfo();
+						//AppinfoMail.updateAppinfo();
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+					loading = false;
+				};
+			}.start();
 		}
 	}
+
+//	private static Document mDocument;
+
+//	static {
+//		try {
+//			String key = AfMD5.getMD5("");
+//			AfDesHelper helper = new AfDesHelper(key );
+//			url = helper.decrypt(url);
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//		}
+//	}
 	
 	public static void initialize() {
 		// TODO Auto-generated method stub
-		new Thread(){
-			public void run() {
-				try {
-					work();
-					AfApplication.getApp().onUpdateAppinfo();
-//					AppinfoMail.updateAppinfo();
-				} catch (Exception e) {
-					// TODO: handle exception
-				}
-			};
-		}.start();
+		loading();
 	}
 
 	/**
@@ -48,53 +63,54 @@ public class AddressKernel {
 	 */
 	public static void work() throws Exception {
 		// TODO Auto-generated method stub
-		mDocument = Jsoup.connect(url).get();
-		Element iframe = mDocument.select("iframe").first();
-		mDocument = Jsoup.connect(iframe.attr("src")).get();
+		String charset = AndRestConfig.getCharset();
+		try {
+			AndRestConfig.setCharset("GBK");
+			DefaultRequestHandler handler = DefaultRequestHandler.getInstance();
+			HttpMethod method = HttpMethod.GET;
+			Response response = handler.doRequest(method , url);
+			Pattern compile = Pattern.compile("<iframe\\s*src=[\"|'](\\S*)[\"|']");
+			Matcher matcher = compile.matcher(response.getBody());
+			if(matcher.find()){
+				String url = matcher.group(1);
+				response = handler.doRequest(method , url);
+				compile = Pattern.compile(">.*\\[(.+)\\].*：([\\u4e00-\\u9fa5]+)\\s*([\\u4e00-\\u9fa5]+)\\s*<");
+				matcher = compile.matcher(response.getBody());
+				if(matcher.find()){
+					address = matcher.group(1);
+					city = matcher.group(2);
+					operator = matcher.group(3);
+				}
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw e;
+		} finally{
+			AndRestConfig.setCharset(charset);
+		}
 	}
 	
 	public static String getAddress() {
 		// TODO Auto-generated method stub
-		if (mDocument != null) {
-			String text = mDocument.text();
-			try {
-				text = text.substring(text.indexOf('[')+1);
-				text = text.substring(0, text.indexOf(']')-1);
-			} catch (Throwable e) {
-				// TODO: handle exception
-			}
-			return text;
+		if (!found) {
+			loading();
 		}
-		return "未获取";
+		return address;
 	}
 	
 	public static String getCity() {
 		// TODO Auto-generated method stub
-		if (mDocument != null) {
-			String text = mDocument.text();
-			try {
-				text = text.substring(text.lastIndexOf("：")+1);
-				text = text.substring(0, text.indexOf(' ')-1);
-			} catch (Throwable e) {
-				// TODO: handle exception
-			}
-			return text;
+		if (!found) {
+			loading();
 		}
-		return "未获取";
+		return city;
 	}
 	
 	public static String getOperator() {
 		// TODO Auto-generated method stub
-		if (mDocument != null) {
-			String text = mDocument.text();
-			try {
-				text = text.substring(text.lastIndexOf(' ')+1);
-			} catch (Throwable e) {
-				// TODO: handle exception
-				return text +":"+e.getClass().toString() +":"+e.getMessage();
-			}
-			return text;
+		if (!found) {
+			loading();
 		}
-		return "未获取";
+		return operator;
 	}
 }
