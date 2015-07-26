98ff4df0d87fdc6f8477e71548f5809f86f18ff2
树朾 <scwang90@hotmail.com>
2015-07-26 23:21:37 +0800

    提交Deploy Model

---
+++ src/com/andcloud/model/Deploy.java
@@ -0,0 +1,71 @@
+package com.andcloud.model;
+
+import com.avos.avoscloud.AVClassName;
+import com.avos.avoscloud.AVObject;
+
+import java.io.Serializable;
+
+@AVClassName("Deploy")
+public class Deploy extends AVObject implements Serializable {
+	/**
+	 * 版本配置
+	 */
+	public static final String Verson = "Verson";
+	public static final String Name = "Name";
+	public static final String Version = "Version";
+	public static final String BusinessModel = "BusinessModel";
+	public static final String Remark = "Remark";
+	public static final String Urls = "Urls";
+
+	public boolean isBusinessModel() {
+		return getBoolean(BusinessModel);
+	}
+
+	public void setBusinessModel(boolean businessModel) {
+		put(BusinessModel,businessModel);
+	}
+
+	public String getName() {
+		return getString(Name);
+	}
+
+	public void setName(String name) {
+		put(Name,name);
+	}
+
+	public String getRemark() {
+		return getString(Remark);
+	}
+
+	public void setRemark(String remark) {
+		put(Remark,remark);
+	}
+
+	public String getUrls() {
+		return getString(Urls);
+	}
+
+	public void setUrls(String urls) {
+		put(Urls,urls);
+	}
+
+	public int getVerson() {
+		return getInt(Version);
+	}
+
+	public void setVerson(int verson) {
+		put(Version,verson);
+	}
+
+	@Override
+	public String toString() {
+		return "OnlineDeploy{" +
+				"BusinessModel=" + isBusinessModel() +
+				", Verson=" + getVerson() +
+				", Name='" + getName() + '\'' +
+				", Version='" + getVerson() + '\'' +
+				", Remark='" + getRemark() + '\'' +
+				", Urls='" + getUrls() + '\'' +
+				'}';
+	}
+}
