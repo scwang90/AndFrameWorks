7fc2d5df2f94385488acac8c1f20dc0fe665c390
scwang90 <scwang90@hotmail.com>
2016-03-30 02:05:30 +0800

    修复框架

--- src/com/andoffice/layoutbind/framework/AbComListItem.java
+++ src/com/andoffice/layoutbind/framework/AbComListItem.java
@@ -1,14 +1,14 @@
 package com.andoffice.layoutbind.framework;
 
-import com.andframe.adapter.AfExpandableAdapter.IAfChildItem;
-import com.andframe.adapter.AfExpandableAdapter.IAfGroupItem;
+//import com.andframe.adapter.AfExpandableAdapter.IAfChildItem;
+//import com.andframe.adapter.AfExpandableAdapter.IAfGroupItem;
 
-public abstract class AbComListItem<T> extends AbListItem<T> implements IAfChildItem<T>,IAfGroupItem<T>{
+public abstract class AbComListItem<T> extends AbListItem<T> /*implements IAfChildItem<T>,IAfGroupItem<T>*/{
 
-	@Override
-	public void onBinding(T model, boolean isExpanded) {
-
-	}
+//	@Override
+//	public void onBinding(T model, boolean isExpanded) {
+//
+//	}
 
 	@Override
 	protected boolean onBinding(T model,int index, SelectStatus status) {
