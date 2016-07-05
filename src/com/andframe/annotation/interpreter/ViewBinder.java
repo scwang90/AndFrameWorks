ace2e93a4139fd401a48240dcfa4a5fda848092a
scwang90 <scwang90@hotmail.com>
2016-07-05 10:36:39 +0800

    优化框架代码

--- src/com/andframe/annotation/interpreter/ViewBinder.java
+++ src/com/andframe/annotation/interpreter/ViewBinder.java
@@ -1,11 +1,16 @@
 package com.andframe.annotation.interpreter;
 
+import android.app.Activity;
+import android.app.Fragment;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.CompoundButton;
+import android.widget.ListView;
 
 import com.andframe.activity.framework.AfView;
 import com.andframe.activity.framework.AfViewable;
@@ -16,10 +21,10 @@ import com.andframe.annotation.view.BindItemClick;
 import com.andframe.annotation.view.BindItemLongClick;
 import com.andframe.annotation.view.BindLayout;
 import com.andframe.annotation.view.BindLongClick;
+import com.andframe.annotation.view.BindTouch;
 import com.andframe.annotation.view.BindView;
 import com.andframe.annotation.view.BindViewModule;
 import com.andframe.application.AfExceptionHandler;
-import com.andframe.feature.framework.EventListener;
 import com.andframe.layoutbind.AfFrameSelector;
 import com.andframe.layoutbind.AfModuleNodata;
 import com.andframe.layoutbind.AfModuleNodataImpl;
@@ -40,9 +45,10 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingQueue;
 
 
@@ -61,15 +67,16 @@ public class ViewBinder {
     }
 
     public static void doBind(AfViewable root) {
-        doBind(root, (AfViewable) root);
+        doBind(root, root);
     }
 
     public static void doBind(Object handler, View root) {
-        doBind(handler, (AfViewable) new AfView(root));
+        doBind(handler, new AfView(root));
     }
 
     public static void doBind(Object handler, AfViewable root) {
         bindClick(handler, root);
+        bindTouch(handler, root);
         bindLongClick(handler, root);
         bindItemClick(handler, root);
         bindItemLongClick(handler, root);
@@ -83,7 +90,27 @@ public class ViewBinder {
         if (handler instanceof AfViewDelegate) {
             return AfViewDelegate.class;
         }
+        if (handler instanceof Activity) {
+            return Activity.class;
+        }
+        if (handler instanceof Fragment) {
+            return Fragment.class;
+        }
         return Object.class;
+    }
+
+    private static void bindTouch(Object handler, AfViewable root) {
+        for (Method method : AfReflecter.getMethodAnnotation(handler.getClass(), getStopType(handler), BindTouch.class)) {
+            try {
+                BindTouch bind = method.getAnnotation(BindTouch.class);
+                for (int id : bind.value()) {
+                    View view = root.findViewById(id);
+                    view.setOnTouchListener(new EventListener(handler).touch(method));
+                }
+            } catch (Throwable e) {
+                AfExceptionHandler.handle(e, TAG(handler, "doBindClick.") + method.getName());
+            }
+        }
     }
 
     private static void bindClick(Object handler, AfViewable root) {
@@ -281,7 +308,7 @@ public class ViewBinder {
     }
 
     public static void bindAfterView(Object handler, AfViewable root) {
-        List<SimpleEntry> methods = new ArrayList<SimpleEntry>();
+        List<SimpleEntry> methods = new ArrayList<>();
         for (Method method : AfReflecter.getMethodAnnotation(handler.getClass(), getStopType(handler), BindAfterViews.class)) {
             BindAfterViews annotation = method.getAnnotation(BindAfterViews.class);
             methods.add(new SimpleEntry(method, annotation));
@@ -313,7 +340,7 @@ public class ViewBinder {
         return null;
     }
 
-    public static class SimpleEntry implements Map.Entry<Method, BindAfterViews> {
+    public static class SimpleEntry {
 
         private final Method key;
         private BindAfterViews value;
@@ -331,16 +358,207 @@ public class ViewBinder {
             return value;
         }
 
-        public BindAfterViews setValue(BindAfterViews object) {
-            BindAfterViews result = value;
-            value = object;
-            return result;
-        }
-
         @Override
         public String toString() {
             return key + "=" + value;
         }
     }
+
+
+    public static class EventListener implements OnClickListener,
+            View.OnLongClickListener,
+            AdapterView.OnItemClickListener,
+            AdapterView.OnItemLongClickListener,
+            CompoundButton.OnCheckedChangeListener, View.OnTouchListener {
+
+        private Object handler;
+
+        private Method clickMethod;
+        private Method touchMethod;
+        private Method longClickMethod;
+        private Method itemClickMethod;
+        private Method itemLongClickMehtod;
+        private Method checkedChangedMehtod;
+
+        public EventListener(Object handler) {
+            this.handler = handler;
+        }
+
+        public OnClickListener click(Method method) {
+            clickMethod = method;
+            return this;
+        }
+
+        public View.OnTouchListener touch(Method method) {
+            touchMethod = method;
+            return this;
+        }
+
+        public View.OnLongClickListener longClick(Method method) {
+            this.longClickMethod = method;
+            return this;
+        }
+
+        public AdapterView.OnItemClickListener itemClick(Method method) {
+            this.itemClickMethod = method;
+            return this;
+        }
+
+        public AdapterView.OnItemLongClickListener itemLongClick(Method method) {
+            this.itemLongClickMehtod = method;
+            return this;
+        }
+
+        public CompoundButton.OnCheckedChangeListener checkedChange(Method method) {
+            this.checkedChangedMehtod = method;
+            return this;
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return Boolean.valueOf(true).equals(invokeMethod(handler, touchMethod, v, event));
+        }
+
+        public void onClick(View v) {
+            invokeMethod(handler, clickMethod, v);
+        }
+
+        public boolean onLongClick(View v) {
+            return Boolean.valueOf(true).equals(invokeMethod(handler, longClickMethod, v));
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            invokeMethod(handler, itemClickMethod, parent, view, position, id);
+        }
+
+        @Override
+        public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+            return Boolean.valueOf(true).equals(invokeMethod(handler, itemLongClickMehtod, parent, view, position, id));
+        }
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+            invokeMethod(handler, checkedChangedMehtod, buttonView, isChecked);
+        }
+
+        private Object invokeMethod(Object handler, Method method, Object... params) {
+            if (handler != null && method != null) {
+                try {
+                    method.setAccessible(true);
+                    return method.invoke(handler, paramAllot(method, params));
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                    AfExceptionHandler.handle(e, "EventListener.invokeMethod");
+                }
+            }
+            return null;
+        }
+
+        /**
+         * 智能参数分配
+         */
+        private Object[] paramAllot(Method method, Object... params) {
+            Set<Integer> set = new HashSet<>();
+            List<Object> list = new ArrayList<>();
+            Class<?>[] types = method.getParameterTypes();
+            if (types.length > 0) {
+                for (int i = 0; i < types.length; i++) {
+                    Object obj = null;
+                    if (params.length > i && params[i] != null && isInstance(types[i], params[i])) {
+                        set.add(i);
+                        obj = params[i];
+                    } else {
+                        for (int j = 0; j < params.length; j++) {
+                            if (params[j] != null && !set.contains(j) && isInstance(types[i], params[j])) {
+                                set.add(j);
+                                obj = params[j];
+                            }
+                        }
+                    }
+                    list.add(obj);
+                }
+            }
+            return paramAllot(method,list.toArray(new Object[list.size()]),params);
+        }
+
+        /**
+         * 特定参数分配
+         */
+        private Object[] paramAllot(Method method, Object[] args, Object... params) {
+            if (params.length == 0) {
+                return args;
+            }
+            Class<?>[] types = null;
+            //View 智能获取tag中的值
+            if (params[0] instanceof View) {
+                Object tag = null;
+                for (int i = 0; i < args.length && i < params.length; i++) {
+                    if (args[i] == null) {
+                        if (tag == null) {
+                            tag = ((View) params[0]).getTag();
+                            if (tag == null) {
+                                break;
+                            }
+                            types = method.getParameterTypes();
+                        }
+                        if (types[i].isAssignableFrom(tag.getClass())) {
+                            args[i] = tag;
+                        }
+                    }
+                }
+            }
+            //ListView 智能获取列表 中的元素
+            if (params[0] instanceof AdapterView && params[2] instanceof Integer) {
+                Adapter adapter = ((AdapterView) params[0]).getAdapter();
+                if (adapter != null && adapter.getCount() > 0) {
+                    int index = ((Integer) params[2]);
+                    if (params[0] instanceof ListView) {
+                        int count = ((ListView) params[0]).getHeaderViewsCount();
+                        index = index >= count ? (index - count) : index;
+                    }
+                    Object value = null;
+                    for (int i = 0; i < args.length && i < params.length; i++) {
+                        if (args[i] == null) {
+                            if (value == null) {
+                                value = adapter.getItem(index);
+                                if (value == null) {
+                                    break;
+                                }
+                                types = method.getParameterTypes();
+                            }
+                            if (types[i].isAssignableFrom(value.getClass())) {
+                                args[i] = value;
+                            }
+                        }
+                    }
+                }
+            }
+            return args;
+        }
+
+        private boolean isInstance(Class<?> t1, Object object) {
+            if (t1.isPrimitive()) {
+                if (t1.equals(int.class)) {
+                    t1 = Integer.class;
+                } else if (t1.equals(short.class)) {
+                    t1 = Short.class;
+                } else if (t1.equals(long.class)) {
+                    t1 = Long.class;
+                } else if (t1.equals(float.class)) {
+                    t1 = Float.class;
+                } else if (t1.equals(double.class)) {
+                    t1 = Double.class;
+                } else if (t1.equals(char.class)) {
+                    t1 = Character.class;
+                } else if (t1.equals(byte.class)) {
+                    t1 = Byte.class;
+                } else if (t1.equals(boolean.class)) {
+                    t1 = Boolean.class;
+                }
+            }
+            return t1.isInstance(object);
+        }
+    }
+
 
 }
