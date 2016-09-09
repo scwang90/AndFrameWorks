41d4d070c8907b520ec5f5bd8107f53f43567e1e
scwang90 <scwang90@hotmail.com>
2016-09-09 15:50:38 +0800

    铺完我的页面

---
+++ src/main/java/com/andpack/fragment/ApListFragment.java
@@ -0,0 +1,45 @@
+package com.andpack.fragment;
+
+import com.andframe.annotation.view.BindViewModule;
+import com.andframe.feature.AfBundle;
+import com.andframe.feature.AfView;
+import com.andframe.fragment.AfListFragment;
+import com.andframe.module.AfModuleTitlebar;
+import com.andpack.api.ApPager;
+import com.andpack.impl.ApPagerHelper;
+
+/**
+ *
+ * Created by SCWANG on 2016/9/7.
+ */
+public abstract class ApListFragment<T> extends AfListFragment<T> implements ApPager {
+
+    @BindViewModule
+    protected AfModuleTitlebar mTitlebar;
+
+    protected ApPagerHelper mHelper = new ApPagerHelper(this);
+
+    @Override
+    protected void onCreated(AfBundle bundle, AfView view) throws Exception {
+        mHelper.onCreate();
+        super.onCreated(bundle, view);
+    }
+
+    @Override
+    public void onAfterViews() throws Exception {
+        mHelper.onAfterViews();
+        super.onAfterViews();
+    }
+
+    //<editor-fold desc="下拉刷新">
+    @Override
+    public boolean onMore() {
+        return false;
+    }
+
+    @Override
+    public boolean onRefresh() {
+        return false;
+    }
+    //</editor-fold>
+}
---
+++ src/main/java/com/andpack/fragment/ApMultiListFragment.java
@@ -0,0 +1,34 @@
+package com.andpack.fragment;
+
+import com.andframe.annotation.view.BindViewModule;
+import com.andframe.feature.AfBundle;
+import com.andframe.feature.AfView;
+import com.andframe.fragment.AfMultiListFragment;
+import com.andframe.module.AfModuleTitlebar;
+import com.andpack.api.ApPager;
+import com.andpack.impl.ApPagerHelper;
+
+/**
+ *
+ * Created by SCWANG on 2016/9/7.
+ */
+public abstract class ApMultiListFragment<T> extends AfMultiListFragment<T> implements ApPager {
+
+    @BindViewModule
+    protected AfModuleTitlebar mTitlebar;
+
+    protected ApPagerHelper mHelper = new ApPagerHelper(this);
+
+    @Override
+    protected void onCreated(AfBundle bundle, AfView view) throws Exception {
+        mHelper.onCreate();
+        super.onCreated(bundle, view);
+    }
+
+    @Override
+    public void onAfterViews() throws Exception {
+        mHelper.onAfterViews();
+        super.onAfterViews();
+    }
+
+}
