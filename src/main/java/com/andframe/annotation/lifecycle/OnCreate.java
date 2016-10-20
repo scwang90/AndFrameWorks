f76991d2d662d5131da42df4d2e1b391a4450c19
scwang90 <scwang90@hotmail.com>
2016-10-20 15:26:21 +0800

    铺完我的页面

--- src/main/java/com/andframe/annotation/lifecycle/OnCreate.java
+++ src/main/java/com/andframe/annotation/lifecycle/OnCreate.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnCreate {
-    int[] value();
+    int[] value() default {};
 }
 
--- src/main/java/com/andframe/annotation/lifecycle/OnCreateView.java
+++ src/main/java/com/andframe/annotation/lifecycle/OnCreateView.java
@@ -12,6 +12,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface OnCreateView {
-    int[] value();
+    int[] value() default {};
 }
 
