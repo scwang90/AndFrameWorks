89517d5078754f21311168aff3f17a8c5e060b20
scwang90 <scwang90@hotmail.com>
2016-11-11 15:37:45 +0800

    添加样式

---
+++ src/main/java/com/andframe/task/AfFeedbackTask.java
@@ -0,0 +1,89 @@
+package com.andframe.task;
+
+import android.support.annotation.NonNull;
+
+import com.andframe.api.Tasker;
+import com.andframe.api.page.Pager;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * 自带反馈的任务
+ * Created by SCWANG on 2016/11/11.
+ */
+
+public class AfFeedbackTask extends AfHandlerTask {
+
+    protected Tasker working;
+    protected Runnable success;
+    protected String intent;
+    protected WeakReference<Pager> mPager;
+
+    public AfFeedbackTask(@NonNull String intent, @NonNull Pager pager) {
+        this(intent, pager, null);
+    }
+
+    public AfFeedbackTask(@NonNull String intent, @NonNull Pager pager, Runnable success) {
+        this.intent = intent;
+        this.mPager = new WeakReference<>(pager);
+        this.success = success;
+    }
+
+    public AfFeedbackTask working(Tasker working) {
+        this.working = working;
+        return this;
+    }
+
+    public AfFeedbackTask success(Runnable success) {
+        this.success = success;
+        return this;
+    }
+
+    @Override
+    protected boolean onPrepare() {
+        showProgressDialog("正在" + intent + "...");
+        return super.onPrepare();
+    }
+
+    @Override
+    protected void onHandle() {
+        hideProgressDialog();
+        if (isFinish()) {
+            makeToastShort(intent + "成功");
+            if (success != null) {
+                success.run();
+            }
+        } else {
+            makeToastShort(makeErrorToast(intent+"失败"));
+        }
+    }
+
+    @Override
+    protected void onWorking() throws Exception {
+        if (working != null) {
+            working.onWorking();
+        }
+    }
+
+    private void showProgressDialog(String message) {
+        Pager pager = mPager.get();
+        if (pager != null) {
+            pager.showProgressDialog(message);
+        }
+    }
+
+    private void makeToastShort(String message) {
+        Pager pager = mPager.get();
+        if (pager != null) {
+            pager.makeToastShort(message);
+        }
+    }
+
+    private void hideProgressDialog() {
+        Pager pager = mPager.get();
+        if (pager != null) {
+            pager.hideProgressDialog();
+        }
+    }
+
+}
