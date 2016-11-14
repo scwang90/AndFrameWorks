faebb9033fb9af12638d763aaeee1ebca5bce2c9
scwang90 <scwang90@hotmail.com>
2016-11-14 10:48:16 +0800

    添加样式

--- src/main/java/com/andpack/annotation/framework/MustChecker.java
+++ src/main/java/com/andpack/annotation/framework/MustChecker.java
@@ -1,5 +1,7 @@
 package com.andpack.annotation.framework;
 
+import android.text.TextUtils;
+
 import com.andframe.annotation.Must;
 import com.andframe.exception.AfToastException;
 import com.andframe.util.java.AfReflecter;
@@ -15,7 +17,8 @@ public class MustChecker {
     public static void checkMust(Object obj) throws Exception {
         for (Field field: AfReflecter.getFieldAnnotation(obj.getClass(), Must.class)) {
             field.setAccessible(true);
-            if (field.get(obj) == null) {
+            Object value = field.get(obj);
+            if (value == null || (value instanceof String && TextUtils.isEmpty(value.toString()))) {
                 throw new AfToastException(field.getAnnotation(Must.class).value());
             }
         }
