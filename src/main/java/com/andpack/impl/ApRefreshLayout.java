524a4447ad7f65fdf79b10177671461c0f046fcb
scwang90 <scwang90@hotmail.com>
2016-11-22 17:31:47 +0800

    铺完我的页面

--- src/main/java/com/andpack/impl/ApRefreshLayout.java
+++ src/main/java/com/andpack/impl/ApRefreshLayout.java
@@ -3,12 +3,13 @@ package com.andpack.impl;
 import android.content.Context;
 import android.content.res.Resources;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewPropertyAnimator;
 
 import com.andframe.api.multistatus.OnRefreshListener;
 import com.andframe.api.multistatus.RefreshLayouter;
 import com.andframe.task.AfDispatcher;
 import com.andframe.util.android.AfDensity;
+import com.andframe.util.java.AfReflecter;
 import com.andpack.R;
 import com.andpack.impl.bezierlayout.BezierLayout;
 import com.lcodecore.tkrefreshlayout.Footer.BottomProgressView;
@@ -23,13 +24,13 @@ import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
  * Created by SCWANG on 2016/10/21.
  */
 
-public class ApRefreshLayout implements RefreshLayouter/*, MoreLayouter*/ {
+public class ApRefreshLayout implements RefreshLayouter<TwinklingRefreshLayout>/*, MoreLayouter*/ {
 
-    private final TwinklingRefreshLayout mTwinkling;
+    private final TwinklingRefreshLayoutEx mTwinkling;
 
     private boolean isRefreshing = false;
-    //    private OnMoreListener mOnMoreListener;
     private OnRefreshListener mOnRefreshListener;
+    //    private OnMoreListener mOnMoreListener;
 
     public ApRefreshLayout(Context context) {
         this(context, R.color.colorPrimary);
@@ -46,7 +47,7 @@ public class ApRefreshLayout implements RefreshLayouter/*, MoreLayouter*/ {
         header.setBackColor(resources.getColor(primaryColorId));
         BottomProgressView bottom = new BottomProgressView(context);
         bottom.setIndicatorColor(resources.getColor(primaryColorId));
-        mTwinkling = new TwinklingRefreshLayout(context);
+        mTwinkling = new TwinklingRefreshLayoutEx(context);
         mTwinkling.setWaveHeight(AfDensity.dp2px(180));
         mTwinkling.setHeaderHeight(AfDensity.dp2px(100));
         mTwinkling.setHeaderView(header);
@@ -55,7 +56,7 @@ public class ApRefreshLayout implements RefreshLayouter/*, MoreLayouter*/ {
         mTwinkling.setOnRefreshListener(twinklingListener);
     }
 
-    public ViewGroup getLayout() {
+    public TwinklingRefreshLayout getLayout() {
         return mTwinkling;
     }
 
@@ -101,9 +102,9 @@ public class ApRefreshLayout implements RefreshLayouter/*, MoreLayouter*/ {
 //        mTwinkling.finishLoadmore();
 //    }
 //
-    TwinklingRefreshLayout.OnRefreshListener twinklingListener = new TwinklingRefreshLayout.OnRefreshListener(){
+    TwinklingRefreshLayout.OnRefreshListener twinklingListener = new com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout.OnRefreshListener(){
         @Override
-        public void onRefresh(TwinklingRefreshLayout refreshLayout) {
+        public void onRefresh(com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout refreshLayout) {
             if (mOnRefreshListener != null) {
                 if (mOnRefreshListener.onRefresh()) {
                     isRefreshing = true;
@@ -126,4 +127,85 @@ public class ApRefreshLayout implements RefreshLayouter/*, MoreLayouter*/ {
 //            }
 //        }
     };
+
+    public ApRefreshLayout setRealContentView(View view) {
+        mTwinkling.setRealContentView(view);
+        return this;
+    }
+
+    public class TwinklingRefreshLayoutEx extends TwinklingRefreshLayout {
+
+        ChildViewWrapper wrapper = null;
+
+        public TwinklingRefreshLayoutEx(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected void onAttachedToWindow() {
+            super.onAttachedToWindow();
+            if (wrapper != null) {
+                wrapperChildView();
+            }
+        }
+
+        private void wrapperChildView() {
+            try {
+                View view = AfReflecter.getPreciseMemberByType(this, View.class);
+                if (view != null && wrapper != view) {
+                    wrapper.setOrginView(view);
+                    AfReflecter.setPreciseMemberByType(this, wrapper, View.class);
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void setRealContentView(View realView) {
+            wrapper = new ChildViewWrapper(realView, realView);
+            wrapperChildView();
+        }
+
+        private class ChildViewWrapper extends View {
+
+            private View view;
+            private View realView;
+
+            public ChildViewWrapper(View view,View realView) {
+                super(view.getContext());
+                this.view = view;
+                this.realView = realView;
+            }
+
+            @Override
+            public float getTranslationY() {
+                return view.getTranslationY();
+            }
+
+            @Override
+            public void setTranslationY(float translationY) {
+                view.setTranslationY(translationY);
+            }
+
+            @Override
+            public ViewPropertyAnimator animate() {
+                return view.animate();
+            }
+
+            @Override
+            public void setOnTouchListener(OnTouchListener l) {
+                view.setOnTouchListener(l);
+            }
+
+            @Override
+            public boolean canScrollHorizontally(int direction) {
+                return realView.canScrollHorizontally(direction);
+            }
+
+            public void setOrginView(View view) {
+                this.view = view;
+            }
+        }
+    }
+
 }
