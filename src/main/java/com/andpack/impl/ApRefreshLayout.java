5c1df7b764d1a80c2ce467466639a9518ee2eec8
wsc <scwang>
2016-11-23 00:02:10 +0800

    添加下拉控件

--- src/main/java/com/andpack/impl/ApRefreshLayout.java
+++ src/main/java/com/andpack/impl/ApRefreshLayout.java
@@ -2,8 +2,16 @@ package com.andpack.impl;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewPropertyAnimator;
+import android.webkit.WebView;
+import android.widget.AbsListView;
+import android.widget.FrameLayout;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.ScrollView;
 
 import com.andframe.api.multistatus.OnRefreshListener;
 import com.andframe.api.multistatus.RefreshLayouter;
@@ -15,7 +23,10 @@ import com.andpack.impl.bezierlayout.BezierLayout;
 import com.lcodecore.tkrefreshlayout.Footer.BottomProgressView;
 import com.lcodecore.tkrefreshlayout.TwinklingRefreshLayout;
 
+import java.util.Collections;
 import java.util.Date;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
@@ -62,8 +73,36 @@ public class ApRefreshLayout implements RefreshLayouter<TwinklingRefreshLayout>/
 
     @Override
     public void setContenView(View content) {
+        View contentView = null;
+        if (content instanceof AbsListView) {
+            contentView = content;
+            FrameLayout layout = new FrameLayout(content.getContext());
+            layout.addView(content, MATCH_PARENT, MATCH_PARENT);
+            content = layout;
+        }
         mTwinkling.addView(content, MATCH_PARENT, MATCH_PARENT);
         mTwinkling.setBackgroundDrawable(content.getBackground());
+        Queue<View> views = new LinkedBlockingQueue<>(Collections.singletonList(content));
+        while (!views.isEmpty() && contentView == null){
+            View view = views.poll();
+            if (view != null) {
+                if (view instanceof ListView
+                        || view instanceof GridView
+                        || view instanceof RecyclerView
+                        || view instanceof ScrollView
+                        || view instanceof WebView) {
+                    contentView = view;
+                } else if (view instanceof ViewGroup) {
+                    ViewGroup group = (ViewGroup) view;
+                    for (int j = 0; j < group.getChildCount(); j++) {
+                        views.add(group.getChildAt(j));
+                    }
+                }
+            }
+        }
+        if (contentView != null && contentView != content && !mTwinkling.hasRealContentView()) {
+            setRealContentView(contentView);
+        }
     }
 
     @Override
@@ -156,14 +195,17 @@ public class ApRefreshLayout implements RefreshLayouter<TwinklingRefreshLayout>/
                     wrapper.setOrginView(view);
                     AfReflecter.setPreciseMemberByType(this, wrapper, View.class);
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
+            } catch (IllegalAccessException ignored) {
             }
         }
 
         public void setRealContentView(View realView) {
             wrapper = new ChildViewWrapper(realView, realView);
             wrapperChildView();
+        }
+
+        public boolean hasRealContentView() {
+            return wrapper != null;
         }
 
         private class ChildViewWrapper extends View {
@@ -198,8 +240,8 @@ public class ApRefreshLayout implements RefreshLayouter<TwinklingRefreshLayout>/
             }
 
             @Override
-            public boolean canScrollHorizontally(int direction) {
-                return realView.canScrollHorizontally(direction);
+            public boolean canScrollVertically(int direction) {
+                return realView.canScrollVertically(direction);
             }
 
             public void setOrginView(View view) {
