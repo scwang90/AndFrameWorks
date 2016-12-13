07fa766e4f9a0cababa59f96e116ec12f265ff20
scwang90 <scwang90@hotmail.com>
2016-12-13 17:33:22 +0800

    完成首页

--- src/main/java/com/andpack/util/ApStatusBarUtil.java
+++ src/main/java/com/andpack/util/ApStatusBarUtil.java
@@ -93,13 +93,13 @@ public class ApStatusBarUtil {
             darkModeForM(window);
         }
 
-        if (Build.VERSION.SDK_INT >= 21) {
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.TRANSPARENT);
-        } else if (Build.VERSION.SDK_INT >= 19) {
-            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+//        } else if (Build.VERSION.SDK_INT >= 19) {
+//            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//        }
 
         setTranslucentView((ViewGroup) window.getDecorView(), color, alpha);
     }
