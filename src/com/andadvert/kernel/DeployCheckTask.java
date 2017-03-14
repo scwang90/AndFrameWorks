952db8d98f9688201a5cdcfb5b698c14a81cc23d
scwang90 <scwang90@hotmaiul.com>
2017-03-14 17:23:18 +0800

    添加新的刷新列表框架

--- src/com/andadvert/kernel/DeployCheckTask.java
+++ src/com/andadvert/kernel/DeployCheckTask.java
@@ -1,28 +1,28 @@
 package com.andadvert.kernel;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import android.content.Context;
-import android.os.Message;
+import android.os.AsyncTask;
 
 import com.andadvert.AdvertAdapter;
 import com.andadvert.model.OnlineDeploy;
+import com.andadvert.util.ACache;
 import com.andframe.application.AfApplication;
 import com.andframe.application.AfExceptionHandler;
 import com.andframe.caches.AfPrivateCaches;
 import com.andframe.exception.AfToastException;
 import com.andframe.helper.java.AfTimeSpan;
-import com.andframe.thread.AfHandlerTask;
 import com.andframe.util.java.AfStringUtil;
 import com.andframe.util.java.AfVersion;
 import com.google.gson.Gson;
 
-public class DeployCheckTask extends AfHandlerTask{
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class DeployCheckTask extends AsyncTask<Void,Void,Void>  {
 
 	/** 是否隐藏广告缓存标记 */
-	public static final String KEY_ISHIDEAD = "70460105144142804102";
+	public static final String KEY_SHOWAD = "70460105144142804102";
 	/** 是否隐藏广告缓存标记 */
 	public static final String KEY_CONFIG = "25791220347152804102";
 
@@ -36,44 +36,45 @@ public class DeployCheckTask extends AfHandlerTask{
 		mContext = context;
 		mAdapter = adapter;
 	}
-	
+
 	@Override
-	public boolean onPrepare() {
-		if (mIsOnlineHideChecking) {
-			return false;
-		}
-		mIsOnlineHideChecking  = true;
-		return super.onPrepare();
-	}
-	
-	@Override
-	protected void onWorking(/*Message msg*/) throws Exception {
-		bedin = new Date();
-		AfTimeSpan span = AfTimeSpan.FromMinutes(5);
-		
-		String deploy = "deploy";
-		deploy = mAdapter.getConfig(mContext, AdvertAdapter.KEY_DEPLOY, deploy);
-		while (deploy.equals("deploy")) {
-			if (AfTimeSpan.FromDate(bedin, new Date()).Compare(span) > 0) {
-				AfPrivateCaches caches = AfPrivateCaches.getInstance();
-				if (!caches.getBoolean(KEY_ISHIDEAD, true)) {
-					mAdapter.helper.setHide(false);
-					return;
-				}
-				//一下异常 会发生 但是概率很低 1%
-				throw new AfToastException("获取在线数据超时");
+	protected Void doInBackground(Void... params) {
+		if (!mIsOnlineHideChecking) {
+			try {
+				mIsOnlineHideChecking  = true;
+				bedin = new Date();
+				int span = 5*60*1000;
+
+				String deploy = "deploy";
+				deploy = mAdapter.getConfig(mContext, AdvertAdapter.KEY_DEPLOY, deploy);
+				while (deploy.equals("deploy")) {
+                    if (System.currentTimeMillis() - bedin.getTime() > span) {
+						ACache cache = ACache.get(mContext);
+						if (String.valueOf(true).equals(cache.getAsString(KEY_SHOWAD))) {
+							mAdapter.helper.setHide(false);
+							return null;
+						}
+                        //以下异常 会发生 但是概率很低 1%
+                        throw new RuntimeException("获取在线数据超时");
+                    }
+                    Thread.sleep(300);
+                    deploy = mAdapter.getConfig(mContext, AdvertAdapter.KEY_DEPLOY, deploy);
+    //			deploy = "{\"Name\":\"poetry\",\"Version\":\"0\",\"HideAd\":false,\"Remark\":\"poetry\",\"Urls\":\"\"}" +
+    //			"|{\"Name\":\"xiaomi\",\"Version\":\"1.7.0.7\",\"HideAd\":true,\"Remark\":\"xiaomi\",\"Urls\":\"\"}" +
+    //			"|{\"Name\":\"baidu\",\"Version\":\"1.7.0.7\",\"HideAd\":true,\"Remark\":\"baidu\",\"Urls\":\"\"}";
+    //			deploy = "{"Name":"poetry","Version":"0","HideAd":false,"Remark":"poetry","Urls":""}" +
+    //			"|{"Name":"xiaomi","Version":"1.7.0.7","HideAd":true,"Remark":"xiaomi","Urls":""}" +
+    //			"|{"Name":"baidu","Version":"1.7.0.7","HideAd":true,"Remark":"baidu","Urls":""}";
+                }
+				this.doAnalysisDeploy(deploy);
+				end = new Date();
+			} catch (Throwable e) {
+				e.printStackTrace();
+			} finally {
+				mIsOnlineHideChecking  = false;
 			}
-			Thread.sleep(300);
-			deploy = mAdapter.getConfig(mContext, AdvertAdapter.KEY_DEPLOY, deploy);
-//			deploy = "{\"Name\":\"poetry\",\"Version\":\"0\",\"HideAd\":false,\"Remark\":\"poetry\",\"Urls\":\"\"}" +
-//			"|{\"Name\":\"xiaomi\",\"Version\":\"1.7.0.7\",\"HideAd\":true,\"Remark\":\"xiaomi\",\"Urls\":\"\"}" +
-//			"|{\"Name\":\"baidu\",\"Version\":\"1.7.0.7\",\"HideAd\":true,\"Remark\":\"baidu\",\"Urls\":\"\"}";
-//			deploy = "{"Name":"poetry","Version":"0","HideAd":false,"Remark":"poetry","Urls":""}" +
-//			"|{"Name":"xiaomi","Version":"1.7.0.7","HideAd":true,"Remark":"xiaomi","Urls":""}" +
-//			"|{"Name":"baidu","Version":"1.7.0.7","HideAd":true,"Remark":"baidu","Urls":""}";
 		}
-		this.doAnalysisDeploy(deploy);
-		end = new Date();
+		return null;
 	}
 
 	public void doAnalysisDeploy(String onlinekey) throws Exception {
@@ -104,15 +105,15 @@ public class DeployCheckTask extends AfHandlerTask{
 	
 	public void doReadCache() {
 		AfPrivateCaches caches = AfPrivateCaches.getInstance();
-		if (!caches.getBoolean(KEY_ISHIDEAD, true)) {
+		if (!caches.getBoolean(KEY_SHOWAD, true)) {
 			mAdapter.helper.setHide(false);
 			return;
 		}
 	}
 	
 	@Override
-	protected void onCancel() {
-		super.onCancel();
+	protected void onCancelled() {
+		super.onCancelled();
 		mIsOnlineHideChecking = false;
 	}
 
@@ -128,10 +129,11 @@ public class DeployCheckTask extends AfHandlerTask{
 		}else if(AfApplication.getApp().isDebug()){
 			String tip = "Waps耗时" + (1.0f*(end.getTime()-bedin.getTime())/1000)+"秒";
 			AfApplication.getApp().getCurActivity().makeToastLong(tip);
+
 		}
 		if(isFinish()){
 			AfPrivateCaches caches = AfPrivateCaches.getInstance();
-			caches.put(KEY_ISHIDEAD, mAdapter.helper.isHide());
+			caches.put(KEY_SHOWAD, mAdapter.helper.isHide());
 			if (!mAdapter.helper.isHide()) {
 				mAdapter.notifyBusinessModelStart(mAdapter.helper.getDeploy());
 			}else {
