7d50dbff05a30ecc14e7ac6248afe4760b8b353b
scwang90 <scwang90@hotmaiul.com>
2017-03-17 15:51:23 +0800

    初始化项目

--- src/com/andcloud/domain/AvDomain.java
+++ src/com/andcloud/domain/AvDomain.java
@@ -1,13 +1,15 @@
 package com.andcloud.domain;
 
 import com.andcloud.model.AvObject;
-import com.andframe.helper.java.AfTimeSpan;
-import com.andframe.util.java.AfReflecter;
 import com.avos.avoscloud.AVException;
 import com.avos.avoscloud.AVObject;
 import com.avos.avoscloud.AVQuery;
 import com.avos.avoscloud.AVQuery.CachePolicy;
 
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AvDomain<T extends AVObject> {
@@ -15,7 +17,7 @@ public class AvDomain<T extends AVObject> {
 	protected Class<T> clazz;
 
 	public AvDomain() {
-		clazz = AfReflecter.getActualTypeArgument(this, AvDomain.class, 0);
+		clazz = getActualTypeArgument(this, AvDomain.class, 0);
 	}
 
 	public AVQuery<T> getQuery(){
@@ -27,7 +29,7 @@ public class AvDomain<T extends AVObject> {
 //		query.setMaxCacheAge(TimeUnit.DAYS.toMillis(1));
 //		query.setMaxCacheAge(AfTimeSpan.FromDays(1).getTotalMilliseconds());
 		query.setCachePolicy(CachePolicy.CACHE_ELSE_NETWORK);
-		query.setMaxCacheAge(AfTimeSpan.FromHours(0.5).getTotalMilliseconds());
+		query.setMaxCacheAge(30 * 60 * 1000);
 		return query;
 	}
 
@@ -77,5 +79,35 @@ public class AvDomain<T extends AVObject> {
 	public void setCachePolicy(CachePolicy policy){
 		AVQuery<T> query = getQuery();
 		query.setCachePolicy(policy);
+	}
+
+	public static <T> Class<T> getActualTypeArgument(Object subobj, Class<?> supclass, int index) {
+		Class<?> subclass = subobj.getClass();
+		List<ParameterizedType> ptypes = new ArrayList<>();
+		ParameterizedType ptype;
+		while (supclass != null && !supclass.equals(subclass)) {
+			Type type = subclass.getGenericSuperclass();
+			if (type == null) {
+				throw new Error("GenericSuperclass not find");
+			} else if (type instanceof Class) {
+				subclass = (Class<?>) type;
+			} else if (type instanceof ParameterizedType) {
+				ptype = ParameterizedType.class.cast(type);
+				ptypes.add(ptype);
+				subclass = (Class<?>) ptype.getRawType();
+			} else if (type instanceof GenericArrayType) {
+				GenericArrayType gtype = GenericArrayType.class.cast(type);
+				subclass = (Class<?>) gtype.getGenericComponentType();
+			} else {
+				throw new Error("GenericSuperclass not case");
+			}
+		}
+		Type type;
+		do {
+			type = ptypes.get(ptypes.size() - 1).getActualTypeArguments()[index];
+			ptypes.remove(ptypes.size() - 1);
+		} while (!(type instanceof Class) && ptypes.size() > 0);
+		//noinspection ConstantConditions
+		return (Class<T>) type;
 	}
 }
