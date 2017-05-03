f0adfa648c10d50a326f3bf9e7bb21109ae181b8
wsc <scwang>
2017-05-03 13:31:40 +0800

    完成了新的大厅页面

--- src/main/java/com/andframe/api/dialog/DialogBuilder.java
+++ src/main/java/com/andframe/api/dialog/DialogBuilder.java
@@ -1,8 +1,11 @@
 package com.andframe.api.dialog;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.database.Cursor;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnDismissListener;
+import android.content.DialogInterface.OnKeyListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ArrayRes;
 import android.support.annotation.AttrRes;
@@ -19,105 +22,37 @@ import android.widget.ListAdapter;
 
 public interface DialogBuilder {
 
-    
     DialogBuilder title(@StringRes int titleId);
-
-    
     DialogBuilder title(CharSequence title);
-    
     DialogBuilder customTitle(View customTitleView);
-
-    
-    DialogBuilder setMessage(@StringRes int messageId);
-
-    
-    DialogBuilder setMessage(CharSequence message);
-
-    
+    DialogBuilder message(@StringRes int messageId);
+    DialogBuilder message(CharSequence message);
     DialogBuilder icon(@DrawableRes int iconId);
-
-
     DialogBuilder icon(Drawable icon);
-
-    
     DialogBuilder iconAttribute(@AttrRes int attrId);
-
-    
-    DialogBuilder positive(@StringRes int textId, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder positive(CharSequence text, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder negative(@StringRes int textId, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder negative(CharSequence text, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder neutraln(@StringRes int textId, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder neutraln(CharSequence text, DialogInterface.OnClickListener listener);
-
-    
+    DialogBuilder positive(@StringRes int textId, OnClickListener listener);
+    DialogBuilder positive(CharSequence text, OnClickListener listener);
+    DialogBuilder negative(@StringRes int textId, OnClickListener listener);
+    DialogBuilder negative(CharSequence text, OnClickListener listener);
+    DialogBuilder neutraln(@StringRes int textId, OnClickListener listener);
+    DialogBuilder neutraln(CharSequence text, OnClickListener listener);
     DialogBuilder cancelable(boolean cancelable);
-
-    DialogBuilder onCancelListener(DialogInterface.OnCancelListener onCancelListener);
-
-    
-    DialogBuilder onDismissListener(DialogInterface.OnDismissListener onDismissListener);
-
-    
-    DialogBuilder onKeyListener(DialogInterface.OnKeyListener onKeyListener);
-
-    
-    DialogBuilder items(@ArrayRes int itemsId, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder items(CharSequence[] items, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder setAdapter(ListAdapter adapter, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder setMultiChoiceItems(@ArrayRes int itemsId, boolean[] checkedItems,
-                                                   DialogInterface.OnMultiChoiceClickListener listener);
-
-    
-    DialogBuilder setMultiChoiceItems(CharSequence[] items, boolean[] checkedItems,
-                                                   DialogInterface.OnMultiChoiceClickListener listener);
-
-    
-    DialogBuilder setMultiChoiceItems(Cursor cursor, String isCheckedColumn, String labelColumn,
-                                                   DialogInterface.OnMultiChoiceClickListener listener);
-
-    
-    DialogBuilder setSingleChoiceItems(@ArrayRes int itemsId, int checkedItem,
-                                       DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder setSingleChoiceItems(Cursor cursor, int checkedItem, String labelColumn,
-                                                    DialogInterface.OnClickListener listener);
-    
-    DialogBuilder setSingleChoiceItems(CharSequence[] items, int checkedItem, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder setSingleChoiceItems(ListAdapter adapter, int checkedItem, DialogInterface.OnClickListener listener);
-
-    
-    DialogBuilder onItemSelectedListener(AdapterView.OnItemSelectedListener listener);
+    DialogBuilder cancelListener(OnCancelListener onCancelListener);
+    DialogBuilder dismissListener(OnDismissListener onDismissListener);
+    DialogBuilder keyListener(OnKeyListener onKeyListener);
+    DialogBuilder items(@ArrayRes int itemsId, OnClickListener listener);
+    DialogBuilder items(CharSequence[] items, OnClickListener listener);
+    DialogBuilder adapter(ListAdapter adapter, OnClickListener listener);
+    DialogBuilder multiChoice(@ArrayRes int itemsId, boolean[] checkedItems, OnMultiChoiceClickListener listener);
+    DialogBuilder multiChoice(CharSequence[] items, boolean[] checkedItems, OnMultiChoiceClickListener listener);
+    DialogBuilder singleChoiceItems(@ArrayRes int itemsId, int checkedItem, OnClickListener listener);
+    DialogBuilder singleChoiceItems(CharSequence[] items, int checkedItem, OnClickListener listener);
+    DialogBuilder singleChoiceItems(ListAdapter adapter, int checkedItem, OnClickListener listener);
+    DialogBuilder selectedListener(AdapterView.OnItemSelectedListener listener);
 
     
     DialogBuilder setView(int layoutResId);
-
-    
     DialogBuilder setView(View view);
-
-    
-    @Deprecated
-    DialogBuilder setView(View view, int viewSpacingLeft, int viewSpacingTop,
-                                       int viewSpacingRight, int viewSpacingBottom);
 
     Dialog create();
     Dialog show();
