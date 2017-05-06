0c7259c297ab29b57044042663ada06d63008e59
wsc <scwang>
2017-05-06 23:02:58 +0800

    完成了新的大厅页面

--- src/main/java/com/andpack/impl/ApExceptionHandler.java
+++ src/main/java/com/andpack/impl/ApExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.andpack.impl;
 
-import com.andframe.application.AfApp;
 import com.andframe.exception.AfExceptionHandler;
 import com.andframe.exception.AfToastException;
-import com.andpack.network.exception.ServerException;
 
 import org.apache.http.conn.ConnectTimeoutException;
 
@@ -20,13 +18,13 @@ public class ApExceptionHandler extends AfExceptionHandler {
 
     @Override
     public String onHandleTip(Throwable e, String tip) {
-        if (e instanceof ServerException) {
+        /*if (e instanceof ServerException) {
             if (AfApp.get().isDebug()) {
                 e = new AfToastException("服务器返回：" + e.getMessage(), e);
             } else {
                 e = new AfToastException("@" + e.getMessage() + "", e);
             }
-        } else if (e instanceof ConnectException || e instanceof UnknownHostException) {
+        } else */if (e instanceof ConnectException || e instanceof UnknownHostException) {
             e = new AfToastException("连接服务器失败", e);
         } else if (e instanceof SocketTimeoutException || e instanceof ConnectTimeoutException) {
             e = new AfToastException("网络请求超时", e);
--- src/main/java/com/andpack/network/retrofit/ServiceApi.java
+++
@@ -1,9 +0,0 @@
-package com.andpack.network.retrofit;
-
-/**
- * ServiceApi
- * Created by SCWANG on 2016/10/28.
- */
-
-public interface ServiceApi {
-}
