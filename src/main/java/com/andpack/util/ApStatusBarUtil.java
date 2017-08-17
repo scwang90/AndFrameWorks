e062b72d667665b35296040a8bfb3c1be10bed94
scwang90 <scwang90@hotmaiul.com>
2017-08-17 18:01:14 +0800

    完成项目管理

--- src/main/java/com/andpack/util/ApStatusBarUtil.java
+++ src/main/java/com/andpack/util/ApStatusBarUtil.java
@@ -1,11 +1,11 @@
 package com.andpack.util;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
 import android.os.Build;
 import android.support.annotation.FloatRange;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
@@ -26,6 +26,7 @@ public class ApStatusBarUtil {
 
     public static int DEFAULT_COLOR = 0;
     public static float DEFAULT_ALPHA = 0;//Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 0.2f : 0.3f;
+    public static final int MIN_API = 19;
 
     //<editor-fold desc="沉侵">
     public static void immersive(Activity activity) {
@@ -61,7 +62,7 @@ public class ApStatusBarUtil {
         } else if (Build.VERSION.SDK_INT >= 19) {
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             setTranslucentView((ViewGroup) window.getDecorView(), color, alpha);
-        } else if (Build.VERSION.SDK_INT >= 16) {
+        } else if (Build.VERSION.SDK_INT >= MIN_API && Build.VERSION.SDK_INT > 16) {
             int systemUiVisibility = window.getDecorView().getSystemUiVisibility();
             systemUiVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
             systemUiVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
@@ -98,14 +99,14 @@ public class ApStatusBarUtil {
         } else if (isMIUI6Later()) {
             darkModeForMIUI6(window, true);
             immersive(window,color,alpha);
-        } else if (Build.VERSION.SDK_INT >= 23) {
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             darkModeForM(window, true);
-            immersive(window,color,alpha);
+            immersive(window, color, alpha);
         } else if (Build.VERSION.SDK_INT >= 19) {
             window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             setTranslucentView((ViewGroup) window.getDecorView(), color, alpha);
         } else {
-            immersive(window,color,alpha);
+            immersive(window, color, alpha);
         }
 //        if (Build.VERSION.SDK_INT >= 21) {
 //            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -121,8 +122,8 @@ public class ApStatusBarUtil {
     //------------------------->
 
     /** android 6.0设置字体颜色 */
-    @TargetApi(Build.VERSION_CODES.M)
-    public static void darkModeForM(Window window, boolean dark) {
+    @RequiresApi(Build.VERSION_CODES.M)
+    private static void darkModeForM(Window window, boolean dark) {
 //        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 //        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 //        window.setStatusBarColor(Color.TRANSPARENT);
@@ -213,14 +214,14 @@ public class ApStatusBarUtil {
 
     /** 增加View的paddingTop,增加的值为状态栏高度 */
     public static void setPadding(Context context, View view) {
-        if (Build.VERSION.SDK_INT >= 16) {
+        if (Build.VERSION.SDK_INT >= MIN_API) {
             view.setPadding(view.getPaddingLeft(), view.getPaddingTop() + getStatusBarHeight(context),
                     view.getPaddingRight(), view.getPaddingBottom());
         }
     }
     /** 增加View的paddingTop,增加的值为状态栏高度 (智能判断，并设置高度)*/
     public static void setPaddingSmart(Context context, View view) {
-        if (Build.VERSION.SDK_INT >= 16) {
+        if (Build.VERSION.SDK_INT >= MIN_API) {
             ViewGroup.LayoutParams lp = view.getLayoutParams();
             if (lp != null && lp.height > 0) {
                 lp.height += getStatusBarHeight(context);//增高
@@ -232,7 +233,7 @@ public class ApStatusBarUtil {
 
     /** 增加View的高度以及paddingTop,增加的值为状态栏高度.一般是在沉浸式全屏给ToolBar用的 */
     public static void setHeightAndPadding(Context context, View view) {
-        if (Build.VERSION.SDK_INT >= 16) {
+        if (Build.VERSION.SDK_INT >= MIN_API) {
             ViewGroup.LayoutParams lp = view.getLayoutParams();
             lp.height += getStatusBarHeight(context);//增高
             view.setPadding(view.getPaddingLeft(), view.getPaddingTop() + getStatusBarHeight(context),
@@ -241,7 +242,7 @@ public class ApStatusBarUtil {
     }
     /** 增加View上边距（MarginTop）一般是给高度为 WARP_CONTENT 的小控件用的*/
     public static void setMargin(Context context, View view) {
-        if (Build.VERSION.SDK_INT >= 16) {
+        if (Build.VERSION.SDK_INT >= MIN_API) {
             ViewGroup.LayoutParams lp = view.getLayoutParams();
             if (lp instanceof ViewGroup.MarginLayoutParams) {
                 ((ViewGroup.MarginLayoutParams) lp).topMargin += getStatusBarHeight(context);//增高
